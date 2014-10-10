a61efc96b31451dda0ab6bef80ed62f375bbed31
Marek Rogala <marek@appsilon.pl>
2014-10-10 22:44:35 +0200

    Add button template

---
+++ memelon/client/views/memes/create_meme_button/create_meme_button.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* CreateMemeButton: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.CreateMemeButton.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.CreateMemeButton.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* CreateMemeButton: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreateMemeButton.created = function () {
+};
+
+Template.CreateMemeButton.rendered = function () {
+};
+
+Template.CreateMemeButton.destroyed = function () {
+};
+
+
