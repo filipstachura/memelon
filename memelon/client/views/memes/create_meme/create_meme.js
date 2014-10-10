6ec3d91c7152b72471d26698aa757d0d06ccdd60
Damian Rodziewicz <damian@appsilon.pl>
2014-10-11 00:01:01 +0200

    Save generated meme to database.

--- memelon/client/views/memes/create_meme/create_meme.js
+++ memelon/client/views/memes/create_meme/create_meme.js
@@ -2,13 +2,19 @@
 /*****************************************************************************/
 /* CreateMeme: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
-
 Template.CreateMeme.events({
-  'keyup input': function (e, tmpl) {
-      var topText = tmpl.find('#top-line').value;
-      var bottomText = tmpl.find('#bottom-line').value;
+  'keyup input': function (event, template) {
+      var topText = template.find('#top-line').value;
+      var bottomText = template.find('#bottom-line').value;
       Meme(Session.get("newPicture"), 'canvas', topText, bottomText);
-   }
+  },
+  'click #save': function(event, template) {
+      var topText = template.find('#top-line').value;
+      var bottomText = template.find('#bottom-line').value;
+      var imageBase64 = template.find("#canvas").toDataURL();
+      Memes.insert({picture: imageBase64, topText: topText, bottomText: bottomText});
+      Router.go("memes.index");
+  }
 });
 
 Template.CreateMeme.helpers({
