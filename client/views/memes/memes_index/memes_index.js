b395495bcd8e0cd5836597896677a6de8d273ab1
Marek Rogala <marek@appsilon.pl>
2014-10-11 12:06:03 +0200

    Select box for circle.

--- client/views/memes/memes_index/memes_index.js
+++ client/views/memes/memes_index/memes_index.js
@@ -3,12 +3,23 @@
 /* MemesIndex: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.MemesIndex.events({
+  'change #selected_circle': function (event, template) {
+    var circleId = template.find('.select-circle-option:selected').value;
+    Router.go('circle.memes', {_id: circleId});
+  }
+
   /*
    * Example:
    *  'click .selector': function (e, tmpl) {
    *
    *  }
    */
+});
+
+Template.CircleSelectOption.helpers({
+  isSelected: function () { 
+    return (name == 'public') ? 'selected': name;
+  }
 });
 
 Template.MemesIndex.helpers({
